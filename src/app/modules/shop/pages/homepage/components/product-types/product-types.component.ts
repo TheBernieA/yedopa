7b632c3ab6989de4ef5d518b656007623d25d271
Bernard Amankwah <bernardamankwah@hotmail.com>
2023-08-09 16:20:31 +0200

    feat:category component

--- src/app/modules/shop/pages/homepage/components/product-types/product-types.component.ts
+++ src/app/modules/shop/pages/homepage/components/product-types/product-types.component.ts
@@ -3,8 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-types',
   templateUrl: './product-types.component.html',
-  styleUrls: ['./product-types.component.scss']
+  styleUrls: ['./product-types.component.scss'],
 })
 export class ProductTypesComponent {
-
+  CATEGORY_DATA: any = [
+    {
+      header_1: 'abs',
+      header_2: 'transmission',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, quisquam.',
+      image: '../../../../../../../assets/images/shop/autoPNG/abs+trans.png',
+      background: 'black',
+      color:'white'
+    },
+    {
+      header_1: 'Scanning',
+      header_2: 'Tools',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, quisquam.',
+      image: '../../../../../../../assets/images/shop/autoPNG/scan-tool.png',
+      background: 'white',
+      color:'black'
+    },
+    {
+      header_1: 'system',
+      header_2: 'system',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, quisquam.',
+      image: '../../../../../../../assets/images/shop/autoPNG/abs+trans.png',
+      background: 'white',
+      color:'black'
+    },
+    {
+      header_1: 'system',
+      header_2: 'system',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, quisquam.',
+      image: '../../../../../../../assets/images/shop/autoPNG/abs+trans.png',
+      background: 'black',
+      color:'white'
+    },
+  ];
 }
--- src/app/modules/shop/shared/navbar/navbar.component.ts
+++ src/app/modules/shop/shared/navbar/navbar.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  NAVLINKS: any = [
+    {
+      label: 'Home',
+      route: '/',
+    },
+    {
+      label: 'Shop',
+      route: '#',
+      subLinks: [
+        {
+          header: 'Auto Body Parts',
+          subLabels: [
+            {
+              label: 'Car Covers',
+              route: '',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: 'Contact Us',
+      route: '/',
+    },
+  ];
 }
